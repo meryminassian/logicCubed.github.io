d52559ee95844e3027ebf66b5b2a1b94cb2e91aa
MovsisyanMher <mher.movsisyan2011@yandex.ru>
2018-12-06 10:36:11 -0800

    Quick-Patch 5

--- src/redirect.js
+++
@@ -1,8 +0,0 @@
-// Made for Logic Cubed
-// We thank you for showing interest :)
-// We exist on Github at https://github.com/meryminas/logicCubed.github.io/
-// We also exist on Facebook:
-// https://www.facebook.com/logicCubed/
-
-
-window.location = "https://meryminas.github.io/logicCubed.github.io/pages/entry.html";
